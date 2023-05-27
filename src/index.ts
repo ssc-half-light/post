d96047ae87f7bd4ad68a79c88b3c7f9358493342
nick thomas <nichoth@gmail.com>
2023-05-26 17:44:43 -0700

    export type for post

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,8 @@ export interface Content {
     mentions:string[]
 }
 
+export type SignedPost = { metadata:SignedMetadata, content:Content }
+
 export async function createContent (
     file:File,
     { text, alt }:{text:string, alt:string}
